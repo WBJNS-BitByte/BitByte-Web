31892afd3ae79d14d1199b8d4a0446bb38eb170d
alohalo2 <wntjd7892@naver.com>
2024-07-16 19:06:21 +0900

    24.07.16 update

--- web/script.js
+++ web/script.js
@@ -1,10 +1,10 @@
 let btn = document.getElementById("sidebar_fold");
 
-btn.addEventListener("click", () => {
+// btn.addEventListener("click", () => {
 
-    btn.classList.add("active");
+//     btn.classList.add("active");
 
-})
+// })
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
