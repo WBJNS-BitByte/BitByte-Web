7491d2844b1e1a8027cde68438ffe244553f2935
ChungDaeun <daeunchungb2m@gmail.com>
2024-09-03 19:26:07 +0900

    240903 정다은-웹진스 springboot 추가

---
+++ WJBNS/src/test/java/com/bit/wjbns/WjbnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.wjbns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjbnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
